47184bb78447d659f37d1e1da3871d4d27184b4c
tungduong <laptung051@gmail.com>
2022-04-30 16:23:16 +0700

    add contract MyTokenSale, CrowdSale

---
+++ section6/contracts/MyTokenSale.sol
@@ -0,0 +1,20 @@
+// contracts/MyToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./CrowdSale.sol";
+
+contract MyTokenSale is CrowdSale{
+
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+
+}
